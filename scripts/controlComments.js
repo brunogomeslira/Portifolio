9ccf722998827633ce95cba438fc4e47acca91c9
Bruno Gomes Lira <brunogomeslira2012@gmail.com>
2019-04-28 13:46:40 -0300

    Add files via upload

---
+++ scripts/controlComments.js
@@ -0,0 +1,126 @@
+
+class controlComments {
+
+  constructor() {
+    let config = {
+      apiKey: "AIzaSyCL70KIMzQraKdmGWevmSfGXodawhUur4k",
+      authDomain: "portifolio-1.firebaseapp.com",
+      databaseURL: "https://portifolio-1.firebaseio.com",
+      projectId: "portifolio-1",
+      storageBucket: "portifolio-1.appspot.com",
+      messagingSenderId: "453946984196"
+    };
+    firebase.initializeApp(config);
+    this._database = firebase.database();
+    this._databaseSet = firebase.database().ref('Coments/');
+    this._databaseGet = firebase.database().ref('Coments/').orderByKey();
+    this._storageRef = firebase.storage().ref();
+  }
+
+  addComentsDisplay(cardsDisplay) {
+    document.getElementById('cards').innerHTML = cardsDisplay;
+  }
+
+  deletar(key, imgref) {
+
+    console.log(key, imgref);
+
+    let _this = this;
+
+    let desertRef = this._storageRef.child(imgref);
+
+    desertRef.delete().then(function () {
+
+      _this._database.ref('Coments/' + key).remove();
+
+    }).catch(function (error) {
+
+    });
+
+  }
+
+  addComents() {
+    event.preventDefault();
+
+    var _this = this;
+    var check = true;
+
+    let fields = document.querySelectorAll("#form-user-create [name]");
+    let user = {};
+    fields.forEach(function (field) {
+
+      if (field.checkValidity()) {
+        user[field.name] = field.value;
+      } else {
+        check = false;
+      }
+
+    });
+
+    if (check) {
+
+      var file = this._file;
+
+      var metadata = {
+        contentType: 'image/jpeg'
+      };
+
+      var uploadTask = this._storageRef.child('images/' + file.name).put(file, metadata);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        function (snapshot) {
+
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          document.getElementById('progress').style.width = progress + '%';
+
+          document.getElementById('progress').innerHTML = progress + '%';
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log('Upload is paused');
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log('Upload is running');
+              break;
+          }
+        }, function (error) {
+
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              break;
+
+            case 'storage/canceled':
+              // User canceled the upload
+              break;
+
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        }, function () {
+
+          user['imgref'] = 'images/' + file.name;
+
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            user.foto = downloadURL;
+            _this._databaseSet.push(user);
+            document.getElementById("form-user-create").reset();
+            document.getElementById('progress').style.width = '0%';
+            document.getElementById('progress').innerHTML = '0%';
+
+          });
+        });
+
+    }
+  }
+
+  getFile(e) {
+    this._file = e.target.files[0];
+  }
+
+}
+
+
+
+
+
---
+++ scripts/siteControl.js
@@ -0,0 +1,29 @@
+window.comments = new controlComments();
+
+window.comments._databaseGet.on("value", function (snapshot) {
+    var cardsDisplay = '';
+    snapshot.forEach(function (childSnapshot) {
+        var key = childSnapshot.key;
+        var childData = childSnapshot.val();
+
+        childData['key'] = key;
+
+        cardsDisplay += 
+            `<div class="col-sm-6 col-md-4">
+              <div class="card mb-5">
+                  <img class="card-img-top" src="${childData.foto}">
+                  <div class="card-body">
+                      <h4 class="card-title">${childData.nome}</h4>
+                      <h6 class="card-subtitle mb-2 text-muted">${childData.nome}</h6>
+                      <p class="card-text">${childData.comentario}
+                      </p>
+                      <button type="button" onclick=" window.comments.deletar('${childData.key}','${childData.imgref}')" class="btn">Deletar</button>
+                  </div>
+              </div>
+              </div>`;
+    });
+
+    window.comments.addComentsDisplay(cardsDisplay);
+}, function (error) {
+   console.log('erro de requerimento');
+});
